f2763eabc76b55bc5b6ab2669113791312f7d560
Vince <giriczvince04@gmail.com>
2026-08-25 03:23:03 +0200

    Residual gating es dropout hozzaadasa

--- models/gated_fusion.py
+++ models/gated_fusion.py
@@ -2,13 +2,14 @@ import torch
 import torch.nn as nn
 
 class GatedFusionModel(nn.Module):
-    def __init__(self, num_dynamic_features=4, num_static_levels=4):
+    def __init__(self, num_dynamic_features=2, num_static_levels=4):
         super(GatedFusionModel, self).__init__()
-
+        
         self.dynamic_encoder = nn.Sequential(
             nn.Conv1d(in_channels=num_dynamic_features, out_channels=32, kernel_size=5, stride=2, padding=2),
             nn.BatchNorm1d(32),
             nn.ReLU(),
+            nn.Dropout(0.2),
             
             nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, stride=2, padding=2),
             nn.BatchNorm1d(64),
@@ -27,22 +28,20 @@ class GatedFusionModel(nn.Module):
         self.classifier = nn.Sequential(
             nn.Linear(64 + 16, 32),
             nn.ReLU(),
-            nn.Dropout(0.3), 
-            nn.Linear(32, 1) 
+            nn.Dropout(0.3),
+            nn.Linear(32, 1)
         )
 
     def forward(self, dynamic_x, static_x):
-        dyn_features = self.dynamic_encoder(dynamic_x)
-        dyn_features = dyn_features.squeeze(-1)
-        
+        dyn_features = self.dynamic_encoder(dynamic_x).squeeze(-1)
         stat_features = self.static_embedding(static_x)
         
-        gate_weights = self.gate_generator(stat_features)
+        gate = self.gate_generator(stat_features)
         
-        gated_dyn_features = dyn_features * gate_weights
-
-        combined_features = torch.cat((gated_dyn_features, stat_features), dim=1)
+        gated_dyn = (dyn_features * gate) + dyn_features
         
-        prediction = self.classifier(combined_features)
+        fused_features = torch.cat((gated_dyn, stat_features), dim=1)
+        
+        prediction = self.classifier(fused_features)
         
         return prediction.squeeze(-1)
